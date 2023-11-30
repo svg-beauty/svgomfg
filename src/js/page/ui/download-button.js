3fc636121e294a41da40ae18970b50ac77055d5a
Rychardt Rothwërkz <redwerkz@gmail.com>
2023-11-30 12:54:20 +0000

    ✏️ Fix typo

--- src/js/page/ui/download-button.js
+++ src/js/page/ui/download-button.js
@@ -10,7 +10,7 @@ export default class DownloadButton extends FloatingActionButton {
       href: './',
       iconSvg:
         // prettier-ignore
-        '<svg aria-hiddem="true" class="icon" viewBox="0 0 24 24">' +
+        '<svg aria-hidden="true" class="icon" viewBox="0 0 24 24">' +
           '<path d="M5 20h14q.425 0 .713.288T20 21q0 .425-.288.713T19 22H5q-.425 0-.712-.288T4 21q0-.425.288-.712T5 20Zm7-2.625q-.225 0-.437-.1t-.363-.3l-4.95-6.35q-.375-.5-.1-1.062T7.05 9H9V3q0-.425.288-.712T10 2h4q.425 0 .713.288T15 3v6h1.95q.625 0 .9.563t-.1 1.062l-4.95 6.35q-.15.2-.363.3t-.437.1Z"/>' +
         '</svg>',
       major: true,
